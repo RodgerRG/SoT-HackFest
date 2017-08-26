2f589e4687ed3896f4306b34f3bff57b73f49fbf
Victor He <vhe821@aucklanduni.ac.nz>
2017-08-27 11:07:18 +1200

    Implemented getLocations to return a list of nearby emergency locations.
    Implemented getSearchResults to match user's keyword for nearby places.

--- src/main/java/googleData/GoogleInteracter.java
+++ src/main/java/googleData/GoogleInteracter.java
@@ -14,40 +14,71 @@ import com.google.maps.PlacesApi;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.GeocodingResult;
 import com.google.maps.model.LatLng;
+import com.google.maps.model.PlaceType;
 import com.google.maps.model.PlacesSearchResponse;
 import com.google.maps.model.PlacesSearchResult;
 
 public class GoogleInteracter {
 	
-	private List<LatLng> locations;
+	private GeoApiContext context;
 	
 	public GoogleInteracter() {
+		context = new GeoApiContext.Builder()
+			    .apiKey("AIzaSyAQiwpugiJrQn0sdELdxj40ukpjr67kj7w")
+			    .build();
 		
 	}
 	
-	
-	
-	 public static void main(String[] args) throws ApiException, InterruptedException, IOException {
+	/**
+	 * 
+	 * Returns a list of emergency places of interests - hospitals, police stations, doctors, pharmacies - 
+	 * within a specified radius boundary from the user's current location.
+	 * 
+	 * 
+	 * @param lat - Latitude of the user's current location.
+	 * @param lng - Longitude of the user's current location.
+	 * @param radius - The radius of the search boundary for nearby Places in metres.
+	 * @return
+	 * @throws ApiException
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
+	public PlacesSearchResult[] getLocations(double lat, double lng, int radius) throws ApiException, InterruptedException, IOException {
+		
+		NearbySearchRequest request = PlacesApi.nearbySearchQuery(context, new LatLng(lat,lng));
 		 
-		 GeoApiContext context = new GeoApiContext.Builder()
-				    .apiKey("AIzaSyAQiwpugiJrQn0sdELdxj40ukpjr67kj7w")
-				    .build();
-		 
-		 NearbySearchRequest request = PlacesApi.nearbySearchQuery(context, new LatLng(-36.848461,174.763336));
-		 
-		 request.radius(10000);
+		 request.radius(radius);
+		 request.type(PlaceType.HOSPITAL,PlaceType.POLICE,PlaceType.DOCTOR,PlaceType.PHARMACY);
 		 
 		 PlacesSearchResponse response = request.await();
 		 PlacesSearchResult[] results = response.results;
 		 
-		 Gson gson = new GsonBuilder().setPrettyPrinting().create();
-			System.out.println(gson.toJson(results));
-		 
-		 /*GeocodingApiRequest request = GeocodingApi.geocode(context, "13A Newhaven Tce, Mairangi Bay, Auckland");
-		 
-		 GeocodingResult[] results = PlacesApi.nearbySearchQuery(context, request);
-				Gson gson = new GsonBuilder().setPrettyPrinting().create();
-				System.out.println(gson.toJson(results[0].addressComponents));*/
-	  }
-
+		 return results;
+	}
+	
+	/**
+	 * 
+	 * Returns a list of results matching the user's search keyword that is within a specified
+	 * radius from the user's location.
+	 * 
+	 * 
+	 * @param lat - Latitude of the user's current location.
+	 * @param lng - Longitude of the user's current location.
+	 * @param radius - The radius of the search boundary for nearby Places in metres.
+	 * @param keyword - 
+	 * @return
+	 * @throws ApiException
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
+	public PlacesSearchResult[] getSearchResults(double lat, double lng, int radius, String keyword) throws ApiException, InterruptedException, IOException {
+		
+		NearbySearchRequest request = PlacesApi.nearbySearchQuery(context, new LatLng(lat,lng));
+		
+		request.radius(radius);
+		
+		request.keyword(keyword);
+		
+		return request.await().results;
+	}
 }
