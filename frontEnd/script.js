30f006456469cd2251b3c5b078bd7bd4c8c71c7e
Derek Kim <wkim505@aucklanduni.ac.nz>
2017-08-26 15:45:32 +1200

    Merge frontend stuff to new repo

---
+++ frontEnd/script.js
@@ -0,0 +1,21 @@
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+	position = {lat: position.coords.latitude, lng: position.coords.longitude};
+    demo.innerHTML = position.lng;
+}
